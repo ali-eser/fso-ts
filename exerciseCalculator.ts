fc608ca7013345c60b847def55eebca552a8cb0a
ali-eser <115903331+ali-eser@users.noreply.github.com>
2023-12-30 22:48:55 +0300

    Finish exercise 9.2

---
+++ exerciseCalculator.ts
@@ -0,0 +1,52 @@
+interface Stats {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+const calculateExercise = (exerciseDays: number[], target: number): Stats => {
+  let totalHours = 0;
+  let trainingDays = 0;
+  let success: boolean;
+  let rating: number;
+  let ratingDescription: string;
+
+  for (let i = 0; i < exerciseDays.length; i++) {
+    totalHours += exerciseDays[i];
+    if (exerciseDays[i] != 0) {
+      trainingDays += 1;
+    }
+  }
+
+  const average: number = totalHours / exerciseDays.length
+
+  if (average >= target) {
+    success = true;
+    rating = 3;
+    ratingDescription = "well done!"
+  } else if (target - average <= 1) {
+    success = false;
+    rating = 2;
+    ratingDescription = "could be better"
+  } else {
+    success = false;
+    rating = 1;
+    ratingDescription = "not enough"
+  }
+
+  return {
+    periodLength: exerciseDays.length,
+    trainingDays: trainingDays,
+    success: success,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average
+  }
+}
+
+console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2));
