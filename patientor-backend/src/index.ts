72ff4efe3c920edcd361f5fc07a3f7210d0dfe42
ali-eser <115903331+ali-eser@users.noreply.github.com>
2024-01-01 21:40:05 +0300

    Finish exercise 9.10

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const cors = require('cors');
+import diagnoseRouter from "./routes/diagnoseEntries";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('this endpoint was pinged');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
